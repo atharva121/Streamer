15e7f02792a7ce1987a6f45b46846423ed0989ac
atharva121 <atharvasaraf@gmail.com>
2018-12-29 14:13:19 +0530

    10th lecture

--- app/src/main/java/com/example/android/streamer/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/android/streamer/Fragments/HomeFragment.java
@@ -1,7 +1,12 @@
 package com.example.android.streamer.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +14,15 @@ import android.view.ViewGroup;
 import com.example.android.streamer.Adapters.HomeRecyclerAdapter;
 import com.example.android.streamer.R;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeSelector
 {
 
     private static final String TAG = "HomeFragment";
+    private RecyclerView mRecyclerView;
+    private HomeRecyclerAdapter mAdapter;
+    private ArrayList<String> mCategories = new ArrayList<>();
 
 
     @Override
@@ -21,9 +31,22 @@ public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeS
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        initRecyclerView(view);
+    }
+
+    private void initRecyclerView(View view) {
+        if (mRecyclerView == null){
+            mRecyclerView = view.findViewById(R.id.recycler_view);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mAdapter = new HomeRecyclerAdapter(mCategories, getActivity(), this);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+    }
 
     @Override
     public void onCategorySelected(int position) {
-
+        Log.d(TAG, "onCategorySelected: list item is clicked!");
     }
 }
