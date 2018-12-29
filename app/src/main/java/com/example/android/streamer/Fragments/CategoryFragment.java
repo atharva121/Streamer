34950c21ab9e79e9676f60a3a52735b4a57082d3
atharva121 <atharvasaraf@gmail.com>
2018-12-29 17:21:23 +0530

    13th lecture

--- app/src/main/java/com/example/android/streamer/Fragments/CategoryFragment.java
+++ app/src/main/java/com/example/android/streamer/Fragments/CategoryFragment.java
@@ -1,19 +1,48 @@
 package com.example.android.streamer.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.streamer.Adapters.CategoryRecyclerAdapter;
+import com.example.android.streamer.Adapters.HomeRecyclerAdapter;
+import com.example.android.streamer.Models.Artist;
 import com.example.android.streamer.R;
 
-public class CategoryFragment extends Fragment
+import java.util.ArrayList;
+
+public class CategoryFragment extends Fragment implements CategoryRecyclerAdapter.ICategorySelector
 {
 
     private static final String TAG = "CategoryFragment";
+    private RecyclerView mRecyclerView;
+    private CategoryRecyclerAdapter mAdapter;
+    private IMainActivity mIMainActivity;
+    private String mSelectedCategory;
+    private ArrayList<Artist> mArtists = new ArrayList<>();
 
+    public static CategoryFragment newInstance(String category){
+        CategoryFragment categoryFragment = new CategoryFragment();
+        Bundle args = new Bundle();
+        args.putString("category", category);
+        categoryFragment.setArguments(args);
+        return categoryFragment;
+    }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null){
+            mSelectedCategory = getArguments().getString("category");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +50,28 @@ public class CategoryFragment extends Fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        initRecyclerView(view);
+    }
 
+    private void initRecyclerView(View view) {
+        if (mRecyclerView == null){
+            mRecyclerView = view.findViewById(R.id.recycler_view);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mAdapter = new CategoryRecyclerAdapter(getActivity(), mArtists, this);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+    }
 
+    @Override
+    public void onArtistSelected(int position) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mIMainActivity = (IMainActivity) getActivity();
+    }
 }
