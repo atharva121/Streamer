5f36143ef2a7a9f2fe65960e289bebc483522512
atharva121 <atharvasaraf@gmail.com>
2019-01-03 16:59:54 +0530

    27th lecture

--- app/src/main/java/com/example/android/streamer/Services/MediaService.java
+++ app/src/main/java/com/example/android/streamer/Services/MediaService.java
@@ -1,5 +1,6 @@
 package com.example.android.streamer.Services;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,9 @@ import android.support.v4.media.MediaBrowserCompat;
 import android.support.v4.media.MediaBrowserServiceCompat;
 import android.support.v4.media.MediaDescriptionCompat;
 import android.support.v4.media.session.MediaSessionCompat;
+import android.text.TextUtils;
+
+import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
 
 import java.util.List;
 
@@ -16,17 +20,42 @@ public class MediaService extends MediaBrowserServiceCompat {
 
     public MediaService() {
         mSession = new MediaSessionCompat(this, TAG);
+        mSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                MediaSessionCompat.FLAG_HANDLES_QUEUE_COMMANDS
+                );
+        mSession.setCallback(new MediaSessionCallback());
+        setSessionToken(mSession.getSessionToken());
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        stopSelf();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSession.release();
     }
 
     @Nullable
     @Override
     public BrowserRoot onGetRoot(@NonNull String s, int i, @Nullable Bundle bundle) {
-        return null;
+        if (s.equals(getApplicationContext().getPackageName())){
+
+        }
+        return new BrowserRoot("empty_media", null);
     }
 
     @Override
     public void onLoadChildren(@NonNull String s, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
-
+        if (TextUtils.equals("empty_media", s)){
+            result.sendResult(null);
+            return;
+        }
+        result.sendResult(null);
     }
 
     public class MediaSessionCallback extends MediaSessionCompat.Callback{
