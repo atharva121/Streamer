29552964685026469a22e52d37030636490de4cf
atharva121 <atharvasaraf@gmail.com>
2019-01-15 23:36:12 +0530

    49th lecture

--- app/src/main/java/com/example/android/streamer/Notificatons/MediaNotificationManager.java
+++ app/src/main/java/com/example/android/streamer/Notificatons/MediaNotificationManager.java
@@ -1,13 +1,22 @@
 package com.example.android.streamer.Notificatons;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.media.MediaDescriptionCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
 
+import com.example.android.streamer.R;
 import com.example.android.streamer.Services.MediaService;
 
 public class MediaNotificationManager {
@@ -46,5 +55,29 @@ public class MediaNotificationManager {
 
     private boolean isAndroidOOrHigher(){
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
+    }
+
+    public Notification buildNotification(@NonNull PlaybackStateCompat state,
+                                          MediaSessionCompat.Token token,
+                                          final MediaDescriptionCompat description,
+                                          Bitmap bitmap){
+        if (isAndroidOOrHigher()){
+            createChannel();
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mMediaService, CHANNEL_ID);
+        builder.setStyle(
+                new android.support.v4.media.app.NotificationCompat.MediaStyle()
+                .setMediaSession(token)
+                .setShowActionsInCompactView(0, 1, 2)
+        )
+                .setColor(ContextCompat.getColor(mMediaService, R.color.notification_bg))
+                .setSmallIcon(R.drawable.ic_audiotrack_white_24dp)
+                .setContentIntent(null)
+                .setContentTitle(description.getTitle())
+                .setContentText(description.getSubtitle())
+                .setLargeIcon(bitmap)
+                .setDeleteIntent(null)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+        return builder.build();
     }
 }
